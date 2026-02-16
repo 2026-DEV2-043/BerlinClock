86f7e5cd2d477ef66682af08d4224423298ca198
2026-DEV2-043 <>
2026-02-16 17:14:49 +0530

    Added test to check topMinuteLightState all top minute lights are On when minute is in between 55 to 60 in BerlinClockMinuteStateTest

--- app/src/main/java/com/berlinclock/domain/model/BerlinClockMinuteState.kt
+++ app/src/main/java/com/berlinclock/domain/model/BerlinClockMinuteState.kt
@@ -1,8 +1,8 @@
 package com.berlinclock.domain.model
 
-import com.berlinclock.constants.HOUR_LIGHT_VALUE
 import com.berlinclock.constants.LightColor
 import com.berlinclock.constants.TOP_MINUTE_LIGHT_COUNT
+import com.berlinclock.constants.TOP_MINUTE_LIGHT_VALUE
 
 class BerlinClockMinuteState {
     private val _topMinuteLightState = MutableList(TOP_MINUTE_LIGHT_COUNT) { LightColor.OFF }
@@ -11,6 +11,7 @@ class BerlinClockMinuteState {
         get() = _topMinuteLightState
 
     fun updateMinuteLightState(minutes: Int) {
-        _topMinuteLightState.forEach { _ -> LightColor.OFF }
+        val topMinuteOnLightCount = minutes / TOP_MINUTE_LIGHT_VALUE
+        (0..< topMinuteOnLightCount).forEach { index -> _topMinuteLightState[index] = LightColor.YELLOW }
     }
 }
--- app/src/test/java/com/berlinclock/domain/model/BerlinClockMinuteStateTest.kt
+++ app/src/test/java/com/berlinclock/domain/model/BerlinClockMinuteStateTest.kt
@@ -29,4 +29,14 @@ class BerlinClockMinuteStateTest {
 
         Assert.assertEquals(true, topMinuteLightState.all {  it == LightColor.OFF })
     }
+
+    @Test
+    fun `check topMinuteLightState all top minute lights are On when minute is in between 55 to 60`() {
+        val minutes = 55
+
+        berlinClockMinuteState.updateMinuteLightState(minutes)
+        val topMinuteLightState = berlinClockMinuteState.topMinuteLightState
+
+        Assert.assertEquals(true, topMinuteLightState.any {  it != LightColor.OFF })
+    }
 }
