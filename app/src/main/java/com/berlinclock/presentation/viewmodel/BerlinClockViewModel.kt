68d236ba6f40417be7d788d134d11995763ffeee
2026-DEV2-043 <>
2026-02-17 17:59:37 +0530

    Fixed Crash issue Due to missing @HiltViewModel annotation

--- app/src/main/java/com/berlinclock/presentation/viewmodel/BerlinClockViewModel.kt
+++ app/src/main/java/com/berlinclock/presentation/viewmodel/BerlinClockViewModel.kt
@@ -5,12 +5,14 @@ import androidx.lifecycle.viewModelScope
 import com.berlinclock.domain.usecase.BerlinClockStateUseCase
 import com.berlinclock.presentation.model.BerlinClockUIState
 import com.berlinclock.utility.toBerlinClockUIState
+import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.SharingStarted
 import kotlinx.coroutines.flow.StateFlow
 import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.stateIn
 import javax.inject.Inject
 
+@HiltViewModel
 class BerlinClockViewModel @Inject constructor(val berlinClockStateUseCase: BerlinClockStateUseCase): ViewModel() {
     val berlinClockState: StateFlow<BerlinClockUIState> by lazy {
         flow {
