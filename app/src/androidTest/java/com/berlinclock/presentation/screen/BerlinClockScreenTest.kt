fbba45165077dd30d9cfd1bb5a5ec246e5eb72c2
2026-DEV2-043 <>
2026-02-17 17:47:19 +0530

    Collecting State of Berlin Clock and Updating the Clock UI, Corrected the BerlinClockScreenTest test case error

--- app/src/androidTest/java/com/berlinclock/presentation/screen/BerlinClockScreenTest.kt
+++ app/src/androidTest/java/com/berlinclock/presentation/screen/BerlinClockScreenTest.kt
@@ -5,6 +5,11 @@ import androidx.compose.ui.test.assertIsNotDisplayed
 import androidx.compose.ui.test.junit4.createComposeRule
 import androidx.compose.ui.test.onNodeWithText
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.berlinclock.constants.BOTTOM_MINUTE_LIGHT_COUNT
+import com.berlinclock.constants.HOUR_LIGHT_COUNT
+import com.berlinclock.constants.LightColorUI
+import com.berlinclock.constants.TOP_MINUTE_LIGHT_COUNT
+import com.berlinclock.presentation.model.BerlinClockUIState
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
@@ -16,16 +21,34 @@ class BerlinClockScreenTest {
 
     @Test
     fun testTitleVisibility_isNotVisible() {
+        val berlinUIClockState= BerlinClockUIState(
+            LightColorUI.OFF,
+            List(HOUR_LIGHT_COUNT) { LightColorUI.OFF },
+            List(HOUR_LIGHT_COUNT) { LightColorUI.OFF },
+            List(TOP_MINUTE_LIGHT_COUNT) { LightColorUI.OFF },
+            List(BOTTOM_MINUTE_LIGHT_COUNT) { LightColorUI.OFF },
+            "00:00:00"
+        )
+
         composeTestRule.setContent {
-            BerlinClockScreen()
+            BerlinClockScreen(berlinUIClockState)
         }
         composeTestRule.onNodeWithText("Title").assertIsNotDisplayed()
     }
 
     @Test
     fun testTitleVisibility_isVisible() {
+        val berlinUIClockState= BerlinClockUIState(
+            LightColorUI.OFF,
+            List(HOUR_LIGHT_COUNT) { LightColorUI.OFF },
+            List(HOUR_LIGHT_COUNT) { LightColorUI.OFF },
+            List(TOP_MINUTE_LIGHT_COUNT) { LightColorUI.OFF },
+            List(BOTTOM_MINUTE_LIGHT_COUNT) { LightColorUI.OFF },
+            "00:00:00"
+        )
+
         composeTestRule.setContent {
-            BerlinClockScreen()
+            BerlinClockScreen(berlinUIClockState)
         }
         composeTestRule.onNodeWithText("Berlin Clock").assertIsDisplayed()
     }
--- app/src/main/java/com/berlinclock/presentation/screen/BerlinClockScreen.kt
+++ app/src/main/java/com/berlinclock/presentation/screen/BerlinClockScreen.kt
@@ -1,6 +1,8 @@
 package com.berlinclock.presentation.screen
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
@@ -12,12 +14,30 @@ import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import com.berlinclock.R
+import com.berlinclock.constants.BOTTOM_MINUTE_LIGHT_COUNT
+import com.berlinclock.constants.HOUR_LIGHT_COUNT
+import com.berlinclock.constants.LightColorUI
+import com.berlinclock.constants.TOP_MINUTE_LIGHT_COUNT
+import com.berlinclock.presentation.component.BerlinClock
+import com.berlinclock.presentation.model.BerlinClockUIState
+import com.berlinclock.presentation.viewmodel.BerlinClockViewModel
+
+@Composable
+fun BerlinClockScreen(
+    berlinClockViewModel: BerlinClockViewModel = hiltViewModel()
+) {
+    val berlinClockUIState = berlinClockViewModel.berlinClockState.collectAsStateWithLifecycle().value
+    BerlinClockScreen(berlinClockUIState)
+}
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun BerlinClockScreen() {
+fun BerlinClockScreen(berlinClockUIState: BerlinClockUIState) {
     Scaffold (modifier = Modifier.fillMaxSize(),
         topBar = {
             TopAppBar(
@@ -32,13 +52,30 @@ fun BerlinClockScreen() {
                 }
             )
         }
-    ) { innerPadding ->
-
+    ) {
+        innerPadding ->
+        Column (modifier = Modifier.padding(innerPadding), content = {
+            BerlinClock(berlinClockUIState)
+            Text(berlinClockUIState.formattedTime,
+                modifier = Modifier.fillMaxWidth().padding(40.dp),
+                textAlign = TextAlign.Center,
+                color = Color.Black,
+                fontSize = 30.sp,
+                fontWeight = FontWeight.Bold)
+        })
     }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun BerlinClockScreenPreview() {
-    BerlinClockScreen()
+    val berlinUIClockState= BerlinClockUIState(
+        LightColorUI.OFF,
+        List(HOUR_LIGHT_COUNT) { LightColorUI.OFF },
+        List(HOUR_LIGHT_COUNT) { LightColorUI.OFF },
+        List(TOP_MINUTE_LIGHT_COUNT) { LightColorUI.OFF },
+        List(BOTTOM_MINUTE_LIGHT_COUNT) { LightColorUI.OFF },
+        "00:00:00"
+    )
+    BerlinClockScreen(berlinUIClockState)
 }
