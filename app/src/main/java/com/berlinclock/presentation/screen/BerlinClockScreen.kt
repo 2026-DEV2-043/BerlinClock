37674b4816143c53a44c403dc1a9d12c4c1adb53
2026-DEV2-043 <>
2026-02-17 16:29:24 +0530

    Refactor BerlinClockScreen to style title text at top

--- app/src/main/java/com/berlinclock/presentation/screen/BerlinClockScreen.kt
+++ app/src/main/java/com/berlinclock/presentation/screen/BerlinClockScreen.kt
@@ -1,13 +1,18 @@
 package com.berlinclock.presentation.screen
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
 import com.berlinclock.R
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -17,7 +22,13 @@ fun BerlinClockScreen() {
         topBar = {
             TopAppBar(
                 title = {
-                    Text(stringResource(R.string.screen_title))
+                    Text(stringResource(R.string.screen_title),
+                        modifier = Modifier.fillMaxWidth(),
+                        textAlign = TextAlign.Center,
+                        color = Color.Black,
+                        fontWeight = FontWeight.SemiBold,
+                        fontSize = 30.sp
+                    )
                 }
             )
         }
